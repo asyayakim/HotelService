b8e6b72de8b29f57fbde6c93d1643fd2a2e2640b
asyayakim <asyayakimova@yahoo.com>
2025-05-06 21:49:57 +0200

    Added LoyalityPoints
    Added auto point system

--- HotelService.Logic/ReservationStatusService.cs
+++ HotelService.Logic/ReservationStatusService.cs
@@ -25,16 +25,31 @@ public class ReservationStatusService : BackgroundService
             using var scope = _services.CreateScope();
             var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
-            var reservationsToUpdate = await dbContext.Reservations
-                .Where(r => r.CheckOutDate < DateOnly.FromDateTime(DateTime.UtcNow) 
-                            && (r.Status == "confirmed" || r.Status == "paid"))
-                .ExecuteUpdateAsync(r => 
-                        r.SetProperty(p => p.Status, "completed"), 
-                    stoppingToken);
+            var reservations = await dbContext.Reservations
+                .Where(r => r.CheckOutDate < DateOnly.FromDateTime(DateTime.UtcNow)
+                            && (r.Status == "confirmed" || r.Status == "paid")).Include(r => r.Customer)
+                .ToListAsync(stoppingToken);
+            foreach (var reservation in reservations)
+            {
+                try
+                {
+                    var totalNight = reservation.CheckOutDate.DayNumber - reservation.CheckInDate.DayNumber;
+                    var points = totalNight * 10;
+                    reservation.Customer.LoyaltyPoints += points;
+                    reservation.Status = "completed";
+                    _logger.LogInformation(
+                        "Added {points} points to customer {customerId} for reservation {reservationId}",
+                        points, reservation.CustomerId, reservation.ReservationId);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error updating reservations");
+                }
 
-            _logger.LogInformation("Updated {count} reservations to completed", reservationsToUpdate);
+                var updatedCount = await dbContext.SaveChangesAsync(stoppingToken);
 
-            await Task.Delay(TimeSpan.FromMinutes(60), stoppingToken);
+                await Task.Delay(TimeSpan.FromMinutes(60), stoppingToken);
+            }
         }
     }
 }
